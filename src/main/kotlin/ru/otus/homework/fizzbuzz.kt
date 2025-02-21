6e03dcb7038f142ceeac2dc58d4ee41ab355bc8d
Sergei Korotaev <zergwsmt@gmail.com>
2025-02-21 18:23:10 +0300

    Initial commit

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,34 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+
+    val a: Array<String?> = arrayOfNulls<String>(n)
+    for (i in 0 until n) {
+        a[i] =
+            if (i%3 == 0) {
+                if (i%5 == 0) {
+                    "FizzBuzz"
+                }
+                else {
+                    "Fizz"
+                }
+            }
+            else {
+                if (i%5 == 0) {
+                    "Buzz"
+                }
+                else {
+                    i.toString()
+                }
+            }
+    }
+    return a.requireNoNulls()
+}
+
+
+fun main() {
+    val d = 32
+    val a = fizzbuzz(d)
+    println("Input: n = $d")
+    println(a.joinToString(", ", "Output: [", "]"))
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,27 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+
+    for (i in numbers.indices) {
+        if (i+1 < numbers.size) {
+            if (numbers[i] + numbers[i+1] == target) {
+                val a = IntArray(2)
+                a[0] = i
+                a[1] = i+1
+                return a
+            }
+        }
+    }
+    throw IllegalArgumentException("target not found")
+}
+
+
+fun main() {
+    val t = 9
+    val n = intArrayOf(9,3,7,3,0,3,9,4,8,2,0,9,3,4,8)
+    val r = sumOfTwo(n, t)
+
+    print(n.joinToString(",", "Input numbers = [", "], "))
+    println("target = $t")
+    println("Output: [${r[0]},${r[1]}]")
 }
